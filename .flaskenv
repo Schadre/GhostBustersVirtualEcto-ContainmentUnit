87773230f56f83f2094d84f25b756f6474bff279
Schadre Dent <schadred@gmail.com>
2024-10-29 00:12:10 -0500

    Updated style.css

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,3 @@
 FLASK_APP=ghostbusters.py
 FLASK_DEBUG=1
+SECRET_KEY='Oriah-Schadre-Saint-555-@@'
--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
+from dotenv import load_dotenv
 
 
 class Config():
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Schadre-555@'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
